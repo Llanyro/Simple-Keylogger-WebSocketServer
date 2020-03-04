44884b898001e2cae2d1ce6937e4f73c4b527281
Francisco Julio <Llanyro@gmail.com>
2020-03-04 18:24:19 +0100

    Update websocketserver.py

--- websocketserver.py
+++ websocketserver.py
@@ -2,12 +2,12 @@ import asyncio
 import websockets
 
 
-async def echo(websocket, path):
+async def servidorEscucha(websocket, path):
     async for message in websocket:
         print(message)
-        await websocket.send(message)
+        await websocket.send("")
 
-start_server = websockets.serve(echo, "localhost", 9090)
+start_server = websockets.servidorEscucha(echo, "localhost", 9090)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
