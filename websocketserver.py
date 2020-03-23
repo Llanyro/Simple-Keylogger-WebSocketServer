372ae0b3535c8d3bc77286253a9303bd4a887522
Francisco Julio <Llanyro@gmail.com>
2020-03-23 15:18:33 +0100

    Update websocketserver.py

--- websocketserver.py
+++ websocketserver.py
@@ -7,7 +7,7 @@ async def servidorEscucha(websocket, path):
         print(message)
         await websocket.send("")
 
-start_server = websockets.serve(servidorEscucha, "localhost", 9090)
+start_server = websockets.serve(servidorEscucha, "localhost", 9090) # Change your ip here if not local
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
